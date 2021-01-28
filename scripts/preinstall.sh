776e2c16d609dfac21478ca0b3f4d37c2d3f2699
4ndyZ <54391621+4ndyZ@users.noreply.github.com>
2021-01-28 12:09:24 +0100

    Fixed loging folder detection and changed installation scripts

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -4,7 +4,4 @@ getent group corona-dashboard >/dev/null || \
 getent passwd corona-dashboard >/dev/null || \
 	useradd -r -g corona-dashboard -s /sbin/nologin \
     -c "User for the Corona-Dashboard Microservice" corona-dashboard
-mkdir -p /var/log/corona-dashboard
-chmod 0644 /var/log/corona-dashboard
-chown corona-dashboard:corona-dashboard /var/log/corona-dashboard
 exit 0
