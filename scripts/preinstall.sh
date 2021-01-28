c29be2df42be65a88c17c3a452c22fc5855f7ea2
4ndyZ <54391621+4ndyZ@users.noreply.github.com>
2021-01-28 11:50:39 +0100

    Added installation scripts for .deb and .rpm packages

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -4,4 +4,7 @@ getent group corona-dashboard >/dev/null || \
 getent passwd corona-dashboard >/dev/null || \
 	useradd -r -g corona-dashboard -s /sbin/nologin \
     -c "User for the Corona-Dashboard Microservice" corona-dashboard
+mkdir -p /var/log/corona-dashboard
+chmod 0644 /var/log/corona-dashboard
+chown corona-dashboard:corona-dashboard /var/log/corona-dashboard
 exit 0
