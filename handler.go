0c03cb4716701e9d795a213c682200658272ff45
4ndyZ <54391621+4ndyZ@users.noreply.github.com>
2021-01-30 15:28:41 +0100

    Added vaccinations
    Added vaccinations data pull and parser
    Added vaccinations dashboard
    Changed data struct

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -9,14 +10,14 @@ import (
 	"time"
 )
 
-func (a *App) GetData(federalState string) (string, error) {
+func (a *App) GetDataCounty(state string) (string, error) {
 	var httpAPI string
-	// API string for all German Federal States
-	if strings.ToLower(federalState) == "all" {
+	// API string for all German Federal states
+	if strings.ToLower(state) == "all" {
 		httpAPI = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=county,BL,cases,cases_per_100k,cases7_per_100k,deaths,last_update&returnGeometry=false&outSR=4326&f=json"
-		// API string for a special German Federal State
+		// API string for a special German Federal state
 	} else {
-		httpAPI = strings.Join([]string{"https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=BL%20%3D%20%27", strings.ToLower(federalState), "%27&outFields=county,BL,cases,cases_per_100k,cases7_per_100k,deaths,last_update&returnGeometry=false&outSR=4326&f=json"}, "")
+		httpAPI = strings.Join([]string{"https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=BL%20%3D%20%27", strings.ToLower(state), "%27&outFields=county,BL,cases,cases_per_100k,cases7_per_100k,deaths,last_update&returnGeometry=false&outSR=4326&f=json"}, "")
 	}
 	// Perform API HTTP call
 	resp, err := http.Get(httpAPI)
@@ -32,39 +33,113 @@ func (a *App) GetData(federalState string) (string, error) {
 	return string(body), nil
 }
 
-func (a *App) ParseData(body *string) ([]Landkreis, error) {
+func (a *App) GetDataVaccination() ([][]string, error) {
+	// Data
+	httpCSV := "https://impfdashboard.de/static/data/germany_vaccinations_timeseries_v2.tsv"
+	// Perform HTTP call
+	resp, err := http.Get(httpCSV)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer resp.Body.Close()
+	reader := csv.NewReader(resp.Body)
+	reader.Comma = '\t'
+	body, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func (a *App) ParseDataCounty(body *string) ([]County, error) {
+	// Constants
+	loc, _ := time.LoadLocation("Europe/Berlin")
+	layout := "02.01.2006, 15:04 Uhr"
 	// Regex to find important data
 	re := regexp.MustCompile(`\{"attributes".*?\}`)
 	// Slice with parsed data
-	var landkreise []Landkreis
+	var counties []County
 	// Search for data using Regex
 	for _, value := range re.FindAllString(*body, -1) {
 		// Cut off suffix
 		value = strings.TrimPrefix(value, `{"attributes":`)
 		// Structs
-		var landkreisjson LandkreisJSON
-		var landkreis Landkreis
+		var countyjson CountyJSON
+		var county County
 		// Unmarshal the JSON data to struct
-		err := json.Unmarshal([]byte(value), &landkreisjson)
+		err := json.Unmarshal([]byte(value), &countyjson)
 		if err != nil {
-			return landkreise, err
+			return counties, err
 		}
 		// Convert JSON struct to my struct
-		landkreis.Name = landkreisjson.Name
-		landkreis.Bundesland = landkreisjson.Bundesland
-		landkreis.Faelle = landkreisjson.Faelle
-		landkreis.FaellePer100k = landkreisjson.FaellePer100k
-		landkreis.FaellePer100k7d = landkreisjson.FaellePer100k7d
-		landkreis.Tode = landkreisjson.Tode
-		// Parse time
-		layout := "02.01.2006, 15:04 Uhr"
-		t, err := time.Parse(layout, landkreisjson.LastUpdate)
+		county.Name = countyjson.Name
+		county.State = countyjson.State
+		county.Cases = countyjson.Cases
+		county.CasesPer100k = countyjson.CasesPer100k
+		county.CasesPer100k7d = countyjson.CasesPer100k7d
+		county.Deaths = countyjson.Deaths
+		// Parse date
+		t, err := time.ParseInLocation(layout, countyjson.LastUpdate, loc)
 		if err != nil {
-			return landkreise, err
+			return counties, err
 		}
-		landkreis.LastUpdate = t
+		county.LastUpdate = t
 		// Append to parsed data the slice
-		landkreise = append(landkreise, landkreis)
+		counties = append(counties, county)
 	}
-	return landkreise, nil
+	return counties, nil
+}
+
+func (a *App) ParseDataVaccination(body *[][]string) ([]Vaccination, error) {
+	// Constants
+	loc, _ := time.LoadLocation("Europe/Berlin")
+	layout := "2006-01-02"
+	// Slice with parsed data
+	var vaccinations []Vaccination
+	//
+	b := *body
+	//
+	for idx, row := range b {
+		// Struct
+		var vaccination Vaccination
+		// Skip first line (CSV header)
+		if idx == 0 {
+			continue
+		}
+		for key, value := range row {
+			// Go through the columns of each row
+			switch b[0][key] {
+			case "dosen_kumulativ":
+				vaccination.Doses.All = a.MustStringToInt(value)
+			case "dosen_biontech_kumulativ":
+				vaccination.Doses.Biontech = a.MustStringToInt(value)
+			case "dosen_moderna_kumulativ":
+				vaccination.Doses.Moderna = a.MustStringToInt(value)
+			case "dosen_astrazeneca_kumulativ":
+				vaccination.Doses.AstraZeneca = a.MustStringToInt(value)
+			case "personen_erst_kumulativ":
+				vaccination.People.FirstTime = a.MustStringToInt(value)
+			case "personen_voll_kumulativ":
+				vaccination.People.Full = a.MustStringToInt(value)
+			case "impf_quote_erst":
+				vaccination.Rate.FirstTime = a.MustStringToFloat(value)
+			case "impf_quote_voll":
+				vaccination.Rate.Full = a.MustStringToFloat(value)
+			case "date":
+				// Parse date
+				t, err := time.ParseInLocation(layout, value, loc)
+				if err != nil {
+					return vaccinations, err
+				}
+				// Add 10h to the date because the they get refresh at 10:00 every day
+				hours, _ := time.ParseDuration("10h")
+				t = t.Add(hours)
+				vaccination.LastUpdate = t
+			default:
+			}
+		}
+		// Append to parsed data the slice
+		vaccinations = append(vaccinations, vaccination)
+	}
+	return vaccinations, nil
 }
